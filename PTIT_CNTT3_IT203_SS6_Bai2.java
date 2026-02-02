17ef90a217d9c8540f07520a1e9256f85b6bda73
hahuydz <haquanghuy2006abc@gmail.com>
2026-02-02 07:17:39 +0700

    bt

--- PTIT_CNTT3_IT203_SS6_Bai2.java
+++ PTIT_CNTT3_IT203_SS6_Bai2.java
@@ -1,4 +1,4 @@
-public class PTIT_CNTT2_IT203_SS6_Bai2 {
+public class PTIT_CNTT3_IT203_SS6_Bai2 {
     public static class Account{
         String username;
         String password;
@@ -32,3 +32,4 @@ public class PTIT_CNTT2_IT203_SS6_Bai2 {
         myAcc.displayProfile();
     }
 }
+
